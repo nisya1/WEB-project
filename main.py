46240f1ab0138617bd88d4341677ef0b499c94fd
Денис <nisya.iv@gmail.com>
2025-03-25 20:41:23 +0300

    добавление модели таблицы

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-from flask import Flask, redirect
-
 import blueprints.movies
+from flask import Flask, redirect
+from flask_wtf.csrf import CSRFProtect
+
 
 app = Flask(__name__)
+
+app.register_blueprint(blueprints.movies.bp)
+
 
 @app.route('/')
 def to_movies():
     return redirect('http://127.0.0.1:8080/movies')
 
-
-app.register_blueprint(blueprints.movies.bp)
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1', debug=True)
