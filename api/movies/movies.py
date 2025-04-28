4d20ee83d29ad53e3c405f1e48d7413784aedb3b
Nikita Golubkov <golubkoffnickita0@gmail.com>
2025-04-28 22:50:48 +0300

    Изменён api фильмов

--- api/movies/movies.py
+++ api/movies/movies.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, abort
 
 from data.db_session import global_init, create_session
 from data.posters_models.events import Events
+from data.posters_models.eventgenre import EventGenre
 
 
 class AllMoviesApi(Resource):
@@ -10,17 +11,19 @@ class AllMoviesApi(Resource):
         global_init(f"database/posters.db")
         sess = create_session()
         movies = sess.query(Events).all()
+        genres = sess.query(EventGenre).all()
         output = {}
 
         for movie in movies:
             cur_data = {}
-            cur_data['title'] = movie.Title
-            cur_data['genre_id'] = movie.GenreId
+            cur_data['genre'] = str(genres[movie.GenreId - 1])
             cur_data['rating'] = movie.Rating
             cur_data['duration'] = movie.Duration
             cur_data['price'] = movie.Price
             cur_data['time'] = movie.Time
+            cur_data['tickets'] = movie.Tickets
+            cur_data['seats'] = movie.Seats
 
-            output[movie.EventId] = cur_data
+            output[movie.Title] = cur_data
 
         return jsonify({"movies": output})
